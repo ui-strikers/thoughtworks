fa6a9b39a398f0b92180154ff392913e3bba327a
Pankaj Tiwari <tiwaripankaj2013@gmail.com>
2019-09-26 13:18:57 +0530

    initial App

---
+++ pankaj-tiwari/exercise10/senoApp/assets/js/seno.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    TW.seno.senoLocal.createOption();
+    TW.seno.senoLocal.selectRegion();
+    TW.seno.senoLocal.createIncludedTable();
+    TW.seno.senoLocal.swapIncludedSeno();
+}
+
+
+
+
+
+
+
+
